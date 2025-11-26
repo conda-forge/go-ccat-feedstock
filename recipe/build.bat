58f01500d5bb4950dfd49aae2c099856b6e27f36
Daniel Nachun <daniel.nachun@gmail.com>
2025-11-26 14:15:04 -0800

    use rattler

--- recipe/build.bat
+++ recipe/build.bat
@@ -3,7 +3,7 @@
 
 set GOPROXY=https://proxy.golang.org || goto :error
 go mod init ccat || goto :error
-go mod tidy || goto :error
+go mod tidy -e || goto :error
 go build -o=%LIBRARY_PREFIX%\bin\ccat.exe -ldflags="-s" || goto :error
 go-licenses save . --save_path=license-files || goto :error
 
--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,6 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 export GOPROXY=https://proxy.golang.org
 go mod init ccat
-go mod tidy
+go mod tidy -e
 go build -o=${PREFIX}/bin/ccat -ldflags="-s -w"
 go-licenses save . --save_path=license-files
